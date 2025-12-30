9757412f50838d7b7652130091ae0d00db9e69d7
jonathan <joni7840850@gmail.com>
2025-12-30 14:45:53 -0600

    Add method to disable/enable and change roles of an user

--- src/main/java/com/payflow/repository/IUserRepository.java
+++ src/main/java/com/payflow/repository/IUserRepository.java
@@ -2,6 +2,8 @@ package com.payflow.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,6 @@ public interface IUserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   boolean existsByEmail(String email);
+
+  Page<User> findAll(Pageable pageable);
 }
