fb934d4327b0081a6bea80280a1a6a4de0441093
jonathan <joni7840850@gmail.com>
2025-12-30 14:46:45 -0600

    Add admin controller

---
+++ src/main/java/com/payflow/controller/AdminController.java
@@ -0,0 +1,77 @@
+package com.payflow.controller;
+
+import com.payflow.DTOS.UserDTO;
+import com.payflow.entity.Role;
+import com.payflow.entity.User;
+import com.payflow.services.UserService;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+@Validated
+public class AdminController {
+
+  private final UserService userService;
+
+  public AdminController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PreAuthorize("hasAnyRole('ADMIN','AUDITOR')")
+  @GetMapping("/users")
+  public ResponseEntity<Page<UserDTO>> getAllUsers(
+      @RequestParam(defaultValue = "0") @Min(0) int page,
+      @RequestParam(defaultValue = "10") @Min(1) int size) {
+
+    Pageable pageable = PageRequest.of(page, size);
+    Page<User> userDTOs = userService.getUsers(pageable);
+    Page<UserDTO> dtoPage = userDTOs.map(u -> new UserDTO(
+        u.getId(),
+        u.getEmail(),
+        u.getFullName(),
+        u.getEnabled(),
+        u.getCreatedAt(),
+        u.getUpdatedAt(),
+        u.getRoles().stream().map(Role::getRole).collect(Collectors.toSet())));
+
+    return ResponseEntity.ok(dtoPage);
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/users/{userId}/disable")
+  public ResponseEntity<String> disableUser(@PathVariable("userId") @Positive long userId) {
+    userService.disableUser(userId);
+    return ResponseEntity.ok("User disabled succesfully");
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/users/{userId}/enable")
+  public ResponseEntity<String> enableUser(@PathVariable("userId") @Positive long userId) {
+    userService.enableUser(userId);
+    return ResponseEntity.ok("User enabled successfully");
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/users/{userId}/roles")
+  public ResponseEntity<String> updateUserRoles(
+      @PathVariable("userId") @Positive long userId,
+      @RequestBody @NotEmpty Set<String> roleNames) {
+    userService.updateUserRoles(userId, roleNames);
+    return ResponseEntity.ok("Roles updated successfully");
+  }
+
+}
