275afc72d240c6bd7c3bbbf556dad8b81ca35aef
jonathan <joni7840850@gmail.com>
2026-01-13 10:06:39 -0600

    Add boostrap infra to save terraform state in s3

---
+++ terraform/bootstrap/outputs.tf
@@ -0,0 +1,14 @@
+output "s3_bucket_name" {
+  description = "S3 bucket for Terraform state"
+  value       = aws_s3_bucket.terraform_state.id
+}
+
+output "dynamodb_table_name" {
+  description = "DynamoDB table for state locking"
+  value       = aws_dynamodb_table.terraform_locks.name
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
